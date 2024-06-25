6d857306d3b60b42113d28e9df1b7532fa09f408
amitshky <amitshkymail@gmail.com>
2024-06-25 16:15:45 +0545

    camera movement updates

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 void CameraController(Camera3D* const camera);
 
 int main(void) {
-    InitWindow(640, 640, "Simple window");
+    InitWindow(640, 640, "Cube");
 
     Camera3D camera = {
         .position = { 100.0f, 100.0f, 200.0f },
@@ -40,7 +40,20 @@ int main(void) {
 }
 
 void CameraController(Camera3D* const camera) {
-    const float distance = 100.0f * GetFrameTime();
+    // press and hold the left mouse button to move the camera
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        HideCursor();
+        DisableCursor();
+    } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+        ShowCursor();
+        EnableCursor();
+    }
+
+    if (!IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+        return;
+
+    // W, A, S, D, E, Q to move the camera's position
+    const float distance = 70.0f * GetFrameTime();
 
     if (IsKeyDown(KEY_A)) { // left
         CameraMoveRight(camera, -distance, true);
@@ -59,4 +72,10 @@ void CameraController(Camera3D* const camera) {
     } else if (IsKeyDown(KEY_Q)) { // back
         CameraMoveUp(camera, -distance);
     }
+
+    // move the mouse to look up/down/left/right
+    const Vector2 delta = GetMouseDelta();
+    const float sensitivity = 0.01f * GetFrameTime();
+    CameraPitch(camera, -delta.y * sensitivity, true, false, false);
+    CameraYaw(camera, -delta.x * sensitivity, false);
 }
